c54e630ce4dfbca8939b3870c31675ae81b72a87
Pedro Demarchi Gomes <pdg16@inf.ufpr.br>
2020-04-17 07:12:04 -0300

    Dont execute nasm when compiler exits with error

--- testes/compilador.sh
+++ testes/compilador.sh
@@ -4,7 +4,10 @@ SRC=$2
 echo "$COMPILADOR $SRC"
 eval "$COMPILADOR $SRC"
 
-echo "nasm -felf64 ${SRC}.s && gcc ${SRC}.o"
-nasm -felf64 ${SRC}.s && gcc ${SRC}.o
-rm ${SRC}.o
+if [ $? -eq 0 ]
+then
+	echo "nasm -felf64 ${SRC}.s && gcc ${SRC}.o"
+	nasm -felf64 ${SRC}.s && gcc ${SRC}.o
+	rm ${SRC}.o
+fi
 
