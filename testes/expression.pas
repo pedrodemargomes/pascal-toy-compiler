784a4ad110bcf624338e6781e3a7a5ad62d07460
Pedro Demarchi Gomes <pdg16@inf.ufpr.br>
2020-04-17 07:57:48 -0300

    Test expressions in write statenent

--- testes/expression.pas
+++ testes/expression.pas
@@ -2,6 +2,9 @@ program teste_expression(input, output);
 Var a, b : integer;
 begin
 	b := 2;
-	a := (b+4)*((2)+2*(2-1*2));
+	a := (b-4)*((2)+2*(2-1*2));
+	write((b-4)*((2)+2*(2-1*2)));
 	write(a);
+	write(0-99);
+	write(b-2);
 end.
