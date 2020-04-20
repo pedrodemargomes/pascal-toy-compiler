58d4a165fb69b9b6bd9bff78eceaba12cc7e7639
Pedro Demarchi Gomes <pedrodemargomes@gmail.com>
2020-04-20 03:53:17 -0300

    Test mod

--- tests/expression.pas
+++ tests/expression.pas
@@ -1,13 +1,13 @@
-program teste_expression(input, output);
+program expression(input, output);
 Var a, b, c : integer;
 begin
 	b := 2;
 	a := (b-4)*((2)+2*(2-1*2));
-	write((b-4)*((2)+2*(2-1*2)));
-	write(a);
-	write(0-99);
-	write(b-2);
-	write((1-3)*(0-2));
+	write((b-4)*((2)+2*(2-1*2))); (*-4*)
+	write(a); (*-4*)
+	write(0-99); (*-99*)
+	write(b-2); (*0*)
+	write((1-3)*(0-2)); (*4*)
 
 	write(99999999);
 
@@ -34,4 +34,25 @@ begin
 	write(b/(1-c)); (*-2*)
 	write((0-b)/(1-c)); (*2*)
 
+	write(99999999);
+
+	a := 9;
+	b := 0-6;
+	write(a); (*9*)
+	write(b); (*-6*)
+	c := a;
+	a := b;
+	b := c;
+	write(a); (*-6*)
+	write(b); (*9*)
+
+	write(99999999);
+
+	write(5 mod 2); (*1*)
+	write((0-5) mod (0-2)); (*-1*)
+	write((0-5) mod (2)); (*-1*)
+	write((5) mod (0-2)); (*1*)
+	write(1+(5+1) mod (2+1)+1); (*2*)
+
+	write(19 mod 4*100); (*300*)
 end.
