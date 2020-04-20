478f74f1d3fe93aa283ac52512865e6798a0f8c9
Pedro Demarchi Gomes <pedrodemargomes@gmail.com>
2020-04-20 03:54:40 -0300

    Test nested ifs

--- tests/if.pas
+++ tests/if.pas
@@ -1,10 +1,20 @@
-program teste_if(input, output);
+program if_(input, output);
 var a,b : integer;
 begin
-	a := 1;
+	a := 2;
    	b := 2;
-	if a > b then
+	if a - b then
 	begin
-		write(a);
+		write(999999);
+	end
+	else
+	begin
+		if 1 then
+		begin
+			write(121212);
+		end;
+		write(0-999999);
 	end;
+	write(a);
+	write(b);
 end.
