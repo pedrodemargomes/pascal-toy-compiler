78f8bcd7443f08302ce6ce38b6a3f8325ef515ff
Pedro Demarchi Gomes <pedrodemargomes@gmail.com>
2020-04-24 21:29:57 -0300

    Test read and write

---
+++ tests/read.pas
@@ -0,0 +1,9 @@
+program read_(input, output);
+var a, b, c : integer;
+begin
+	read(a, b, c);
+	writeln(a);
+	writeln(b);
+	writeln(c);
+end.
+
---
+++ tests/write.pas
@@ -0,0 +1,9 @@
+program write(input, output);
+var a, b : integer;
+begin
+	a := 42;
+	b := 43;
+	writeln(a);
+	writeln(b);
+end.
+
